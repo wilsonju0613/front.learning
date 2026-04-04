a19336296e1b408dc279f1903bd331ffffcba9af
wilson <jowilsonm123055602@gmail.com>
2026-04-05 00:40:24 +0800

    module practice

---
+++ lib.js
@@ -0,0 +1,11 @@
+function echo(msg){
+            console.log(msg);
+           }
+function add(n1,n2){
+    console.log(n1+n2);
+}
+// export default 資料 
+export default {
+    echo:echo,
+    add:add
+};
---
+++ main.js
@@ -0,0 +1,10 @@
+//import 資料 from "模組檔案的路徑";
+import lib from "./lib.js";
+console.log(lib);
+lib.echo("我是wilson")
+lib.add(3,4);
+/*
+import echo from "./lib.js";
+let name="我又大又肥";
+echo(name);
+*/
